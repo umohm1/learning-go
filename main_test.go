8e223aceedfad96601ed1e878ab58b2cf498c2b0
Mary Umoh <mumoh@bcorporation.net>
2019-08-23 17:10:23 -0400

    Test for nonexistent route

--- main_test.go
+++ main_test.go
@@ -73,3 +73,31 @@ func TestHandler(t *testing.T) {
 		}
 
 	}
+
+	func TestRouterForNonExistentRoute(t *testing.T) {
+		r := newRouter()
+		mockServer := httptest.NewServer(r)
+		resp, err := http.Post(mockServer.URL+ "/hello", "", nil)
+
+		if err != nil {
+			t.Fatal(err) 
+		}
+
+		// set status to 405 (method not allowed)
+		if resp.StatusCode != http. StatusMethodNotAllowed {
+			t.Errorf("Status should be 405, got %d", resp.StatusCode)
+
+		}
+
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		respString := string(b)
+		expected := ""
+
+		if respString != expected {
+			t.Errorf("Response should be %s, got %s", expected, respString)
+		}
+	}
