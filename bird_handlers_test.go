ae23e605adec9c7b1abdfaf69be10fde0e9f2cb1
Mary Umoh <mumoh@bcorporation.net>
2019-09-04 16:31:43 -0400

    Add to bird handler test

--- bird_handlers_test.go
+++ bird_handlers_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -40,4 +42,49 @@ func TestGetBirdsHandler(t *testing.T) {
 	if actual != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
 	}
+}
+
+func TestCreateBirdsHandler(t *testing.T) {
+
+	birds = []Bird{
+		{"sparrow", "Small bird"},
+	}
+
+	form := newCreateBirdForm()
+	req, err := http.NewRequest("POST", "", bytes.NewBufferString(form.Encode()))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Iota(len(form.Encode())))
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+
+	hf := http.HandleFunc(createBirdHandler)
+
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := Bird{"eagle", "A bird of prey"}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := birds[1]
+
+	if actual != expected {
+		t.Error("handler returned unexpected body: got %v want %v", actual, expected)
+	}
+}
+
+func newCreateBirdForm() *url.Values {
+	form := url.Values{}
+	form.Set("species", "eagle")
+	form.Set("description", "A bird of prey")
+	return &form
 }
