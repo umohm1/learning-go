adac113d689f85b6000a0f26473ab4931889e580
Mary <umohm12@gmail.com>
2019-10-07 16:45:32 -0400

    Add store to application

--- bird_handlers.go
+++ bird_handlers.go
@@ -14,17 +14,25 @@ type Bird struct {
 var birds []Bird 
 
 func getBirdHandler(w http.ResponseWriter, r *http.Request) {
-// convert the birds var into json 
-birstListBytes, err := json.Marshal(birds)
 
-//If there is an error, print it to the console, and return a server
-// error response to the user
-if err != nil {
-	fmt.Println(fmt.Errorf("Error: %v", err))
-	w.WriteHeader(http.StatusInternalServerError)
+	/*
+		The list of birds is now taken from the store instead of the package level  `birds` variable we had earlier
+
+		The `store` variable is the package level variable that we defined in
+		`store.go`, and is initialized during the initialization phase of the
+		application
+	*/
+
+  birds, err := store.GetBirds()
+
+	// Everything else is the same as before
+	birdListBytes, err := json.Marshal(birds)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	// write the JSON list of birds to the response
 	w.Write(birdListBytes)
 }
 
@@ -46,7 +54,10 @@ func createBirdHandler(w http.ResponseWriter, r *http.Request) {
 	bird.Description = r.Form.Get("description")
 
 	//Append existing list of birds w/ a new entry
-	birds = append(birds, bird)
+	err = store.CreateBird(&bird)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	//Redirect user to HTML page using Redirect method
 	http.Redirect(w, r, "/assets/", http.StatusFound)
